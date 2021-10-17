e02588057133b68c8e1773e53a5d638d83e6c789
Elaine Wang <elaine35002@gmail.com>
2021-10-17 13:17:31 +0800

    wirte file

--- products.py
+++ products.py
@@ -1,4 +1,4 @@
-product = []
+products = []
 while True:
 	name = input('請輸入商品名稱: ')	
 	if name == 'q':
@@ -9,8 +9,14 @@ while True:
 	# # p.append(price)
 	# p = [name, price] #等於7~9行
 	# product.append(p) 
-	product.append([name, price]) #等於10~11行
-print(product)
+	products.append([name, price]) #等於10~11行
+print(products)
 
 for p in products:
 	print(p[0], '的價格是', p[1])
+
+
+# with 自動close
+with open('products.csv', 'w') as f:
+	for p in products:
+		f.write(p[0] + ',' + p[1] + '\n')
